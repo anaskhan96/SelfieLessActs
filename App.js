5116496d1aecd04947a75c2fc1124480113403e4
Anas Khan <anas.khan96@outlook.com>
2019-03-22 00:49:24 +0530

    landing page of the app

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight, Alert, TouchableWithoutFeedback, Keyboard, Image } from 'react-native';
+import { verticalScale, moderateScale } from './dimensions';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      teamId: "",
+      ipAddress: ""
+    }
+  }
+
+  onLogIn = () => {
+    Alert.alert("TeamId entered is " + this.state.teamId);
+    Alert.alert("IP Addr is " + this.state.ipAddress);
+  }
+
   render() {
     return (
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/selfie_icon.png')} style={{width: 100, height: 100}}/>
+        <Text style={styles.text}>Welcome to SelfieLessActs!</Text>
+        <TextInput style={styles.textInput} placeholder="Enter your TEAM ID" onChangeText={(teamId) => this.setState({teamId})} />
+        <TextInput style={styles.textInput} placeholder="Enter your IP" onChangeText={(ipAddress) => this.setState({ipAddress})}/>
+        <TouchableHighlight style={styles.button} onPress={this.onLogIn}>
+          <Text style={{fontSize: 17}}>LOG IN</Text>
+        </TouchableHighlight>
       </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -15,7 +37,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+    fontSize: 17,
+    padding: 10,
+  },
+  textInput: {
+    fontSize: 17,
+    paddingTop: 10,
+    paddingBottom: 10,
+    textAlign: 'center',
+  },
+  button: {
+    height: 40,
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
+  }
 });
