fe29cb9509be7c8505da2dd0840111b985f6ae8b
Anas Khan <anas.khan96@outlook.com>
2019-04-07 22:50:21 +0530

    adding bare standalone camera

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Modal, StyleSheet, Text, View, TextInput, TouchableHighlight, Alert, TouchableWithoutFeedback, Keyboard, Image, AsyncStorage, Button } from 'react-native';
+import { Modal, StyleSheet, Text, View, TextInput, TouchableHighlight, Alert, TouchableWithoutFeedback, Keyboard, Image, AsyncStorage, Button, TouchableOpacity } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Menu, { MenuItem } from 'react-native-material-menu';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Camera, Permissions } from 'expo';
 
 class LoginScreen extends React.Component {
   static navigationOptions = {
@@ -117,7 +118,7 @@ class HomeScreen extends React.Component {
           }
           this.setState({ categories: categories });
         }).catch(err => {
-          console.error(err);
+          console.log(err);
           Alert.alert('Error fetching categories :- ' + err);
         });
       });
@@ -202,7 +203,7 @@ class HomeScreen extends React.Component {
             <Text style={{ fontSize: 17 }}>LOG OUT</Text>
           </TouchableHighlight>
           <TouchableHighlight style={styles.button} onPress={this.addImage}>
-            <Text style={{ fontSize: 17 }}>ADD IMAGE</Text>
+            <Text style={{ fontSize: 17 }} onPress={() => this.props.navigation.navigate("Camera", { name: "Camera" })}>ADD IMAGE</Text>
           </TouchableHighlight>
         </View>
         <Modal
@@ -226,6 +227,59 @@ class HomeScreen extends React.Component {
         </Modal>
       </View>
     )
+  }
+}
+
+class CameraScreen extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
   }
 }
 
@@ -261,6 +315,7 @@ const styles = StyleSheet.create({
 const MainNavigator = createStackNavigator({
   Login: { screen: LoginScreen },
   Home: { screen: HomeScreen },
+  Camera: { screen: CameraScreen },
 });
 
 const App = createAppContainer(MainNavigator);
